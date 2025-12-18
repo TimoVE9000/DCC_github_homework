bf60b29e0ea598091e71ef0299dd2b2e8e68c5fb
TimoVE9000 <timovaneldijk@gmail.com>
2025-12-18 15:12:09 +0100

    adding python function

--- main_script.R
+++ main_script.R
@@ -1,17 +1,26 @@
 #This script executes all the number processing functions
+install.packages("reticulate")
+library(reticulate)
 
 #Loading all the functions into memory, add your function here
 source("fun_TimoVE9000.R")
+reticulate::source_python("pyfun_TimoVE9000/tjbfun.py")
 
 #the input is defined here
-data = c (1,2,3,4,5)
-print (data)
+data = c(1,2,3,4,5)
+print(data)
 
+#R Function by TimoVE9000, print result
+data = fun_TimoVE9000(data)
+print(data)
 
-#Function by TimoVE9000, print result
+#R Function by TimoVE9000, print result
 data = fun_TimoVE9000(data)
 print (data)
 
+#Python Function by TimoVE9000, print result
+data=tjbfun(data)
+print(data)
 
 
 
